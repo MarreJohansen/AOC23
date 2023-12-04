b8a1bfa0ca44540e360b8723d3e49863359f9e70
Martin Johansen <marjo253@student.liu.se>
2023-12-04 19:49:44 +0100

    update

--- puzzle 1/1.py
+++ puzzle 1/1.py
@@ -5,30 +5,19 @@ def main():
     with open("puzzle 1/input.txt") as f:
         for line in f:
             listOfStartIndexes = {}
-            #print(line)
             for key in alphToNumb:
-                #print(key)
                 if (key in line):
-                    #print("ok")
                     listOfStartIndexes[line.find(key)] = key
-                    #line = line.replace(key, alphToNumb.get(key))
             sortedIndexes = dict(sorted(listOfStartIndexes.items()))
-            print(sortedIndexes)
             for index in sortedIndexes:
                 word = sortedIndexes.get(index)
                 line = line.replace(word, alphToNumb.get(word))
-            print(line)
             total += int(loopy(line) + loopy(reversed(line)))
-            #print(total)
     return total
 
 def loopy(line):
-    print(line)
     for i in line:
         if(i.isdigit()):
-            return i
-        
-#def changeVal():
-    
+            return i   
 
 print(main())
